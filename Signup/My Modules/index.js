d8efbe59924e7a68c38db80826d44dfa48d4bfc3
jagrati-c <panchaljagrati1997@gmail.com>
2021-04-14 14:16:50 +0530

    functionality done

--- Signup/My Modules/index.js
+++ Signup/My Modules/index.js
@@ -24,35 +24,35 @@ function signup(cont) {
 
     <div>
     <label class="name-label">First name</label><br/>
-<input type="text" class="nameInpt"/>
+<input type="text" class="nameInpt" id="first-name"/>
     </div>
 
     <div>
     <label class="name-label">Last name</label><br/>
-    <input type="text" class="nameInpt"/>
+    <input type="text" class="nameInpt" id="last-name"/>
     </div>
 
     </div>
 
     <div class="margin">
     <label class="name-label">Email address</label><br/>
-    <input type="email" class="nameInpt email-input"/>
+    <input type="email" class="nameInpt email-input" id="email"/>
     </div>
 
     <div class="margin">
     <label class="name-label">Username
     <a class="color-grey">(only letters, numbers, and underscores)</a></label><br/>
-    <input type="username" class="nameInpt email-input"/>
+    <input type="username" class="nameInpt email-input" id="user-name"/>
     </div>
 
     <div class="margin">
     <label class="name-label">Password
     <a class="color-grey">(min. 6 char)</a></label><br/>
-    <input type="password" class="nameInpt email-input"/>
+    <input type="password" class="nameInpt email-input" id="password"/>
     </div>
 
     <div class="margin">
- <button class="join-btn">Join</button>
+ <button class="join-btn" onclick="join()" id="join-btn">Join</button>
     </div>
 
     <div class="term-privacy-text" class="margin">
@@ -61,3 +61,4 @@ function signup(cont) {
     `
 // return signupBox;
 }
+
