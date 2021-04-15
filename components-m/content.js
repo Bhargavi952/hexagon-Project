88d4bfea715a26ef724de7a34c34b92dad245252
Rohit Rawat <rohitrawat4242@gmail.com>
2021-04-15 12:44:41 +0530

    main photo

--- components-m/content.js
+++ components-m/content.js
@@ -1,10 +1,12 @@
+
+
 function content(){
     let parent = document.createElement("div")
     parent.setAttribute("id","photos-main-div")
 
     window.addEventListener("scroll", function(){
         let {scrollTop,scrollHeight,clientHeight} = document.documentElement
-        // console.log({scrollTop,scrollHeight,clientHeight})
+        console.log({scrollTop,scrollHeight,clientHeight})
         if(scrollTop + clientHeight == scrollHeight){
             getData()
         }
@@ -15,17 +17,37 @@ function content(){
         .then(res => res.json())
         .then(res => {
             for(let i = 0; i < res.length; i++){
-                let {urls:{regular}} = res[i]
+                let {urls:{small}} = res[i]
                 let div = document.createElement("div")
                 div.setAttribute("id","photo-div")
                 let img = document.createElement("img")
-                img.src = regular
+                img.src = small
                 img.setAttribute("id","photo")
-                div.append(img)
+                let btn = document.createElement("button")
+                btn.innerHTML = `like`
+                btn.addEventListener("click",function(){
+
+                    let fileName = myImageLike(small)
+                    saveAs(small,fileName)
+                })
+                btn.setAttribute("id","photo-btn")
+                let btn1 = document.createElement("button")
+                btn1.innerHTML = `collection`
+                btn1.setAttribute("id","photo-btn1")
+                div.append(img,btn,btn1)
                 parent.append(div)
             }
             console.log(res)
         })
+        function myImageLike(){
+            // let a = document.createElement("a")
+            // a.setAttribute("href","data:text/plain;charset=utf-8," + encodeURIComponent(text))
+            // a.setAttribute("download",k)
+            // a.style.display = "none"
+            // document.body.appendChild(a)
+            // a.click()
+            // document.body.removeChild(a)
+        }
     }
 
     getData()
@@ -35,3 +57,4 @@ function content(){
 
 
 export default content
+
---
+++ components-m/main-photo.js
@@ -0,0 +1,16 @@
+function mainPhoto(){
+    let mainPhotoDiv = document.createElement("div")
+    mainPhotoDiv.setAttribute("id", "main-photo")
+    mainPhotoDiv.innerHTML = `
+        <img src="https://images.unsplash.com/photo-1615818499660-30bb5816e1c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80" alt="">
+        <div id="main-photo-content">
+            <span>Unsplash</span>
+            <h1>The internet’s source of <a href="#">freely-usable images.</a> <br>Powered by creators everywhere.</h1>
+            <input type="text" placeholder="Search free high-resolution photos">
+            <p>Trending:flowerwallpapersbackgroundshappylove</p>
+        </div>
+    `
+    return mainPhotoDiv
+}
+
+export default mainPhoto
